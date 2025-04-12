49a24ea009c3da97efa46f04b85b14177f2fd1c8
edRibeiro <edersonribeiro.ita@gmail.com>
2025-04-12 10:09:31 -0300

    feat: Desafio - Controle de Fluxo

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import Exceptions.ParametrosInvalidosException;
+
+public class Contador {
+    public static void main(String[] args) throws Exception {
+        Scanner terminal = new Scanner(System.in);
+        int parametroUm = 0;
+        int parametroDois = 0;
+
+        try {
+            System.out.println("Digite o primeiro parâmetro:");
+            parametroUm = terminal.nextInt();
+
+            System.out.println("Digite o segundo parâmetro:");
+            parametroDois = terminal.nextInt();
+            contar(parametroUm, parametroDois);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Você deve digitar apenas números inteiros.");
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
---
+++ DesafioControleFluxo/src/Exceptions/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
